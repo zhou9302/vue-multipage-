f8b2a704e4f407be32e2aa76aa06d513becfb9f8
周颖 <zhouying@zhouyingdeMacBook-Pro.local>
2018-08-29 17:00:51 +0800

    完成

--- src/pages/duban/mock/index.js
+++ src/pages/duban/mock/index.js
@@ -124,7 +124,7 @@ Mock.mock('/api/obpm/countPendingItems?leaderId=&officeId=&pageStart=1&num=4', '
       ITEM_PARTAPPROACH: '我委会办',
       'ID': '@guid',
       ITEM_TASKSUMMARY: '2018年重大事项督办管理办法修改后的',
-      offifcename: '综合调研处',
+      officeName: '综合调研处',
       OPERATOR: '小周周'
     }]
   }
